9441e8273ef5f1074d4956acfa5ff830b5bd4d85
Amar Waterloo <am.wmare@gmail.com>
2025-09-09 17:16:08 -0400

    Calculator Project

---
+++ Calculator.py
@@ -0,0 +1,31 @@
+print("Lets start with addition of two numbers")
+First_input = input("Enter first value: ") #First Value
+F_input = int(First_input)
+Second_input = input("Second value: ")# Second Value
+S_input = int(Second_input)
+Addition = F_input + S_input
+print("Addition: ",Addition ) # Additiona of two numbers
+
+print("Now how about exploring subtraction")
+First_input = input("Enter first value: ") #First Value
+F_input = int(First_input)
+Second_input = input("Second value: ")# Second Value
+S_input = int(Second_input)
+Subtraction = F_input - S_input
+print("Subtraction: ",Subtraction)
+
+print("Moving to multiplication")
+First_input = input("Enter first value: ") #First Value
+F_input = int(First_input)
+Second_input = input("Second value: ")# Second Value
+S_input = int(Second_input)
+Multiplication = F_input * S_input
+print("Multiplication:",Multiplication)
+
+print("Lastly Division")
+First_input = input("Enter first value: ") #First Value
+F_input = int(First_input)
+Second_input = input("Second value: ")# Second Value
+S_input = int(Second_input)
+Division = F_input/S_input
+print("Division:", int(Division))
