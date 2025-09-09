cefc5011de43c18c36b504542d026276d7915238
Amar Waterloo <am.wmare@gmail.com>
2025-09-09 16:05:12 -0400

    Age Project

---
+++ Age.py
@@ -0,0 +1,7 @@
+startyear = input("Enter created year")
+startyear_int = int(startyear)
+#print(startyear_int)
+endyear = input("Enter current year")
+endyear_int = int(endyear)
+Age = endyear_int - startyear_int
+print(Age)
