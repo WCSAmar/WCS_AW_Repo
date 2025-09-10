ead692823b051fd2a485e5c9a2849596257f9fbc
Amar Waterloo <am.wmare@gmail.com>
2025-09-09 21:26:37 -0400

    Circle Project

---
+++ Circle.py
@@ -0,0 +1,9 @@
+
+radius = int(input("Radius: "))
+pi_value = 3.14
+area = pi_value * (radius ** 2)
+circumference = 2 * pi_value * radius
+print("Area:", area)
+print("Circumference:", circumference)
+
+
