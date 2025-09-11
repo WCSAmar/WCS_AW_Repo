827424d43cf75bf52fa3995c31d1822dcb61bee4
Amar Waterloo <am.wmare@gmail.com>
2025-09-10 21:20:06 -0400

    Fibonacci Sequence

---
+++ Fibonacci.py
@@ -0,0 +1,20 @@
+#Fibonacci Sequence
+v1 = 0
+v2 = 1
+#Fibonacci numbers needed
+n = int(input("User Input:"))
+
+for int in range(n):
+    temp = v1
+    print(temp, end=" " )
+    v1 = v2
+    v2 = temp + v2
+    
+
+
+ 
+
+
+
+ 
+    
