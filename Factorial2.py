ada51626479348c195d4aec7366ca6a1d8a8ca9f
Amar Waterloo <am.wmare@gmail.com>
2025-09-16 06:19:39 -0400

    Factorial using recursion

---
+++ Factorial2.py
@@ -0,0 +1,9 @@
+#Write a function to calculate factorials using recursion.
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+print("Factorial:", factorial(5))
